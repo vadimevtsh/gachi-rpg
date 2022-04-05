d44a14390f868e2412a5f8be58f3c12fcf5b75a2
Vadim Yevtushenko <76635569+vadimevtsh@users.noreply.github.com>
2022-04-05 16:19:04 +0300

    Add files via upload

---
+++ Components/Game/Timer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+	float totalSeconds = 0;
+	float elapsedSeconds = 0;
+
+	bool running = false;
+	bool started = false;
+		
+	public float Duration
+    {
+		set
+        {
+			if (!running)
+            {
+				totalSeconds = value;
+			}
+		}
+	}
+
+	public bool Finished
+    {
+		get { return started && !running; } 
+	}
+	
+	public bool Running
+    {
+		get { return running; }
+	}
+
+    void Update()
+    {	
+		if (running)
+        {
+			elapsedSeconds += Time.deltaTime;
+			if (elapsedSeconds >= totalSeconds)
+            {
+				running = false;
+			}
+		}
+	}
+
+	public void Run()
+    {	
+		if (totalSeconds > 0)
+        {
+			started = true;
+			running = true;
+            elapsedSeconds = 0;
+		}
+	}
+	
+}
